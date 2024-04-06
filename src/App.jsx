76a21c7beabc60cc23e8c2c339e0fcd36be2b1cf
tiennguyen275 <tienmn2705@gmail.com>
2024-04-06 18:53:04 +0800

    MUI Config Theme, Typography..

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,18 @@ import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
 import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
 import HomeIcon from '@mui/icons-material/Home'
 import { pink } from '@mui/material/colors'
+import Typography from '@mui/material/Typography'
 
 function App() {
 
   return (
     <>
       <div>Testing</div>
+
+      <Typography variant="body2" color="text.secondary">Testing Typography</Typography>
+
       <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
+      <Button variant="contained" color="success">Contained</Button>
       <Button variant="outlined">Outlined</Button>
 
       <br />
