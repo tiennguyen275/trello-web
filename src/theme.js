558f687edca88c241e96068755be299c2e539675
tiennguyen275 <tienmn2705@gmail.com>
2024-04-11 17:32:06 +0700

    Responsive and scroll bar design

--- src/theme.js
+++ src/theme.js
@@ -29,6 +29,24 @@ const theme = extendTheme({
   },
   components: {
     // Name of the component
+    MuiCssBaseline: {
+      styleOverrides: {
+        body: {
+          '*::-webkit-scrollbar': {
+            width: '8px',
+            height: '8px'
+          },
+          '*::-webkit-scrollbar-thumb': {
+            backgroundColor: '#bdc3c7',
+            borderRadius: '8px'
+          },
+          '*::-webkit-scrollbar-thumb:hover': {
+            backgroundColor: '#8A2BE2',
+            borderRadius: '8px'
+          }
+        }
+      }
+    },
     MuiButton: {
       styleOverrides: {
         // Name of the slot
